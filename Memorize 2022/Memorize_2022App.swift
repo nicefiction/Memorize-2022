0559918a29fe873f61b1128e75d347a1ec6ae3c6
Olivier Van hamme <nicefiction@gmail.com>
2022-05-12 17:19:53 +0200

    Initial Commit

---
+++ Memorize 2022/Memorize_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_2022App.swift
+//  Memorize 2022
+//
+//  Created by Olivier Van hamme on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
