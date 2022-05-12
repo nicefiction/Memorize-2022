4499d5c01566b81ca176405e42ee610478f133f8
Olivier Van hamme <nicefiction@gmail.com>
2022-05-12 19:05:51 +0200

    Completes Lecture 1.

--- Memorize 2022/ContentView.swift
+++ Memorize 2022/ContentView.swift
@@ -1,21 +1,49 @@
-//
-//  ContentView.swift
-//  Memorize 2022
-//
-//  Created by Olivier Van hamme on 12/05/2022.
-//
-
+// MARK: LIBRARIES
 import SwiftUI
 
+
+
 struct ContentView: View {
+
+    // MARK: - STATIC PROPERTIES
+    // MARK: - PROPERTY WRAPPERS
+    // MARK: - PROPERTIES
+    // MARK: - COMPUTED PROPERTIES
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.00)
+                .stroke(lineWidth: 3)
+            Text("Hello 👋 World 🌍")
+                .font(.title)
+                .fontWeight(.regular)
+                
+        }
+        .padding(.horizontal)
+        .foregroundColor(Color.red)
+            
     }
+    
+    
+    
+    // MARK: - STATIC METHODS
+    // MARK: - INITIALIZERS
+    // MARK: - METHODS
+    // MARK: - HELPER METHODS
 }
 
+
+
+
+
+
+// PREVIEWS ///////////////////////////////////
 struct ContentView_Previews: PreviewProvider {
+    
+    // MARK: - STATIC PROPERTIES
+    // MARK: - COMPUTED PROPERTIES
     static var previews: some View {
+        
         ContentView()
     }
 }
--- Memorize 2022/Memorize_2022App.swift
+++ Memorize 2022/Memorize_2022App.swift
@@ -1,17 +1,46 @@
-//
-//  Memorize_2022App.swift
-//  Memorize 2022
-//
-//  Created by Olivier Van hamme on 12/05/2022.
-//
-
+// MARK: LIBRARIES
 import SwiftUI
+
+
 
 @main
 struct Memorize_2022App: App {
+    
+    // MARK: - STATIC PROPERTIES
+    // MARK: - PROPERTY WRAPPERS
+    // MARK: - PROPERTIES
+    // MARK: - COMPUTED PROPERTIES
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
+    
+    
+    // MARK: - STATIC METHODS
+    // MARK: - INITIALIZERS
+    // MARK: - METHODS
+    // MARK: - HELPER METHODS
 }
+
+
+
+
+
+
+// MARK: LIBRARIES
+
+// MARK: - STATIC PROPERTIES
+// MARK: - PROPERTY WRAPPERS
+// MARK: - PROPERTIES
+// MARK: - COMPUTED PROPERTIES
+// MARK: - STATIC METHODS
+// MARK: - INITIALIZERS
+// MARK: - METHODS
+// MARK: - HELPER METHODS
+
+// PREVIEWS /////////////////
+
+// MARK: - STATIC PROPERTIES
+// MARK: - COMPUTED PROPERTIES
